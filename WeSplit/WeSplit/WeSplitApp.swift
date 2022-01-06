fb10a4cce4671abc415ea47bcb22ff5176f46057
Harry-KNIGHT <elliotharryk@gmail.com>
2022-01-06 20:53:07 +0100

    Ading first project

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Elliot knight on 06/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
