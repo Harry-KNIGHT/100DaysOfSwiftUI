abca8531fc819b3d6ace981d069db5dbc707651e
Harry-KNIGHT <elliotharryk@gmail.com>
2022-01-16 19:37:17 +0100

    custom modifiers extension

--- ViewsAndModifiersCodeExemple/ViewsAndModifiersCodeExemple/ContentView.swift
+++ ViewsAndModifiersCodeExemple/ViewsAndModifiersCodeExemple/ContentView.swift
@@ -7,10 +7,48 @@
 
 import SwiftUI
 
+
+struct Title: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.largeTitle)
+            .foregroundColor(.white)
+            .padding()
+            .background(.blue)
+            .clipShape(RoundedRectangle(cornerRadius: 20))
+    }
+}
+
+extension View {
+    func titleStyle() -> some View {
+        modifier(Title())
+    }
+}
+
+// Modifiers added in view for use them again multiple time
+struct CapsuleText: View {
+    var text: String
+    var body: some View {
+        Text(text)
+            .padding()
+            .font(.largeTitle)
+            .foregroundStyle(.regularMaterial)
+            .background(.blue)
+            .clipShape(Capsule())
+        
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(spacing: 20) {
+            // Using CapsuleText
+            CapsuleText(text: "Elliot")
+            CapsuleText(text: "Arthur")
+            Text("Bonbons de gimauve")
+//                .modifier(Title())
+                .titleStyle()
+        }
     }
 }
 
