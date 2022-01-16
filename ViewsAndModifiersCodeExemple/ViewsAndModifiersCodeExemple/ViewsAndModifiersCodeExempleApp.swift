6a71d605eaaccf685725faad2805e13de8bc8e70
Harry-KNIGHT <elliotharryk@gmail.com>
2022-01-16 18:35:01 +0100

    Modifier in view for rusing them

---
+++ ViewsAndModifiersCodeExemple/ViewsAndModifiersCodeExemple/ViewsAndModifiersCodeExempleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersCodeExempleApp.swift
+//  ViewsAndModifiersCodeExemple
+//
+//  Created by Elliot knight on 16/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersCodeExempleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
