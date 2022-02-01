8dc9fae47eadf95f29d18b00ff845e8331454c67
Harry-KNIGHT <elliotharryk@gmail.com>
2022-02-01 11:41:15 +0100

    Word Scramble (Day30)

---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Elliot Knight on 01/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
